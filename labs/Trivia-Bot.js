565f204f9763ee34d910be807bbefcbe38912cc7
dev-Rukaiya-Salim <rukaiyasalim41@gmail.com>
2026-05-06 15:51:39 +0300

    feat: created Trivia Bot lab

---
+++ labs/Trivia-Bot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+const  botName = "PkayPkay";
+
+const botLocation = "KayIsland";
+
+const botNameIntroduction = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(botNameIntroduction);
+
+let favoriteLanguage = "Java";
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I enjoy coding in " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "One thing that I like about " + favoriteLanguage + " is its versatility " + ".";
+console.log(codingFact);
+
+codingFact = "Learning " + favoriteLanguage + " has been fun and interesting " + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
