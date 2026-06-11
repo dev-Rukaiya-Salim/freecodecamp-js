7fc1f5fb2264e08fda769d817970145d530606dd
dev-Rukaiya-Salim <rukaiyasalim41@gmail.com>
2026-06-11 17:09:24 +0300

    feat: build a celsius to fahrenheit converter

---
+++ labs/Celsius-Fahrenheit-converter.js
@@ -0,0 +1,13 @@
+function convertCtoF(celsius) {
+  return (celsius * 9/5) +32;
+}
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(0));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));
+
+
+
