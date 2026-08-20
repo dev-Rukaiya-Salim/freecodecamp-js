a6d3306e307adf9c01fa6dd45a3338b3a7725fd0
dev-Rukaiya-Salim <rukaiyasalim41@gmail.com>
2026-08-20 14:20:16 +0300

    feat: example code

---
+++ data-structures/stacks/stack.js
@@ -0,0 +1,11 @@
+let stack = [];
+
+stack.push("Plate 1");
+stack.push("Plate 2");
+stack.push("Plate 3");
+
+stack.pop();
+
+console.log("stack before pop:", stack);
+
+console.log("stack after pop:", stack);
